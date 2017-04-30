8c921684739b31fdfff07fef1cbff99f04c74d15
Marvin Rekovsky <rekovsky@stud.hs-heilbronn.de>
2017-04-30 20:13:51 +0200

    Cameraview implemented, not tested on actual device yet

--- app/src/main/java/com/example/user/aib_labswp_2017_ss_entnahmeapp/PrimerList.java
+++ app/src/main/java/com/example/user/aib_labswp_2017_ss_entnahmeapp/PrimerList.java
@@ -1,9 +1,19 @@
 package com.example.user.aib_labswp_2017_ss_entnahmeapp;
 
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.os.Bundle;
+import com.google.android.gms.vision.CameraSource;
+
+import java.io.IOException;
 
 /**
  * Created by Marvin on 30.04.2017.
@@ -11,12 +21,67 @@ import android.os.Bundle;
 public class PrimerList extends AppCompatActivity {
 
     private Spinner spinner;
+    private SurfaceView cameraPreview;
     private static final String[] items = {"Entnommen", "Nicht entnommen"};
+    private CameraSource cameraSource;
+    private final int RequestCameraPermissionID = 1001;
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode)
+        {
+            case RequestCameraPermissionID:
+            {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+                        return;
+                    }
+                    try {
+                        cameraSource.start(cameraPreview.getHolder());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.primerlist);
+
+        cameraPreview = (SurfaceView) findViewById(R.id.cameraPreview);
+        cameraPreview.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+                    ActivityCompat.requestPermissions(PrimerList.this, new String[]{Manifest.permission.CAMERA}, RequestCameraPermissionID);
+                    return;
+                }
+
+                try {
+                    cameraSource.start(cameraPreview.getHolder());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                cameraSource.stop();
+            }
+        });
 
         spinner = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(PrimerList.this,
