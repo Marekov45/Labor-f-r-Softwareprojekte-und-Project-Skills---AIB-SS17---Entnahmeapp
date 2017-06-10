dd8ea17cbd7cf1f78a83802ed857ced4c5bb7746
Marvin Rekovsky <rekovsky@stud.hs-heilbronn.de>
2017-06-10 16:44:29 +0200

    specified that there's no Actionbar for the laboratory GUI

--- app/src/main/java/client_aib_labswp_2017_ss_entnahmeapp/View/View/LaborGui.java
+++ app/src/main/java/client_aib_labswp_2017_ss_entnahmeapp/View/View/LaborGui.java
@@ -2,17 +2,30 @@ package client_aib_labswp_2017_ss_entnahmeapp.View.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.SearchView;
 import client.aib_labswp_2017_ss_entnahmeapp.R;
 
 public class LaborGui extends AppCompatActivity {
 
 
     private Button logoutButton;
+    private SearchView view;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_labor_gui);
+
+        view = (SearchView) findViewById(R.id.search);
+        view.setQueryHint("Primername/Primerart");
+        logoutButton = (Button) findViewById(R.id.logout);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
