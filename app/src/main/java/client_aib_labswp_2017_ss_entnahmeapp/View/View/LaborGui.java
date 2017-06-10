742282bfd6e1ef56a8508828782f7acf8d01476e
Marvin Rekovsky <rekovsky@stud.hs-heilbronn.de>
2017-06-10 16:08:35 +0200

    Implemented GUI for laboratory

--- app/src/main/java/client_aib_labswp_2017_ss_entnahmeapp/View/View/LaborGui.java
+++ app/src/main/java/client_aib_labswp_2017_ss_entnahmeapp/View/View/LaborGui.java
@@ -2,10 +2,14 @@ package client_aib_labswp_2017_ss_entnahmeapp.View.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import client.aib_labswp_2017_ss_entnahmeapp.R;
 
 public class LaborGui extends AppCompatActivity {
 
+
+    private Button logoutButton;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
