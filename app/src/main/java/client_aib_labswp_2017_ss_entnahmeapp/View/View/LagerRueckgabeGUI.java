4171ce53aef9f8926df320fff29b233389888544
Vanessa Neumann <vneumann@stud.hs-heilbronn.de>
2017-06-13 19:59:43 +0200

    Login Gui überarbeitet, Logout Button in LaborGUi implementiert

--- app/src/main/java/client_aib_labswp_2017_ss_entnahmeapp/View/View/LagerRueckgabeGUI.java
+++ app/src/main/java/client_aib_labswp_2017_ss_entnahmeapp/View/View/LagerRueckgabeGUI.java
@@ -1,14 +1,35 @@
 package client_aib_labswp_2017_ss_entnahmeapp.View.View;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.*;
 import client.aib_labswp_2017_ss_entnahmeapp.R;
+import client_aib_labswp_2017_ss_entnahmeapp.View.Controller.ServerAPI.CustomObserver;
+import client_aib_labswp_2017_ss_entnahmeapp.View.Controller.ServerAPI.ListImpl;
+import client_aib_labswp_2017_ss_entnahmeapp.View.Controller.enumResponseCode.ResponseCode;
+import client_aib_labswp_2017_ss_entnahmeapp.View.Model.User;
+import client_aib_labswp_2017_ss_entnahmeapp.View.Model.model_List.PickList;
+import client_aib_labswp_2017_ss_entnahmeapp.View.Model.model_List.PrimerTube;
+import com.google.android.gms.vision.barcode.Barcode;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LagerRueckgabeGUI extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lager_rueckgabe_gui);
+
     }
-}
+
+};
