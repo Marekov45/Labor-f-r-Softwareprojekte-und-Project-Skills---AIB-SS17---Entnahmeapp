af40b03932e1898d771d33ede5b895497a9cfdec
BENNI\Benjamin <bsteiger@stud.hs-heilbronn.de>
2017-06-16 15:47:48 +0200

    implemented JUnit Tests

---
+++ app/src/test/java/ServerAPITest/ServerAPITestSuit.java
@@ -0,0 +1,20 @@
+package ServerAPITest;
+
+import org.junit.Test;
+
+/**
+ * If you run this class, the ListImplTest class and the LoginControllerImplTest class will be start.
+ * Created by Benjamin on 16.06.2017.
+ */
+public class ServerAPITestSuit {
+
+
+    @Test
+    public void testAllTests(){
+
+        ListImplTest lit = new ListImplTest();
+        LoginControllerImplTest lci = new LoginControllerImplTest();
+
+    }
+
+}
