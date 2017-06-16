8e899bc883c0d2ce42981b9d363e15b276d5d2f0
lhaeusse <lhaeusse@stud.hs-heilbronn.de>
2017-06-16 21:36:23 +0200

    RestRequessts implementiert

--- app/src/main/java/client_aib_labswp_2017_ss_entnahmeapp/View/Controller/ServerAPI/PrimerAPI.java
+++ app/src/main/java/client_aib_labswp_2017_ss_entnahmeapp/View/Controller/ServerAPI/PrimerAPI.java
@@ -1,6 +1,9 @@
 package client_aib_labswp_2017_ss_entnahmeapp.View.Controller.ServerAPI;
 
+import client_aib_labswp_2017_ss_entnahmeapp.View.Model.model_List.Location;
+import client_aib_labswp_2017_ss_entnahmeapp.View.Model.model_List.PrimerTube;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
@@ -14,8 +17,16 @@ public interface PrimerAPI {
     Call<Void> takePrimer(@Path("id") long id, @Path("username") String username, @Path("password") String password);
 
     @PUT("/app/location/{id}/{username}/{password}")
-    Call<Void> setLocation(@Path("id") long id, @Path("username") String username, @Path("password") String password);
+    Call<Void> sendLocation(@Path("id") long id, @Path("username") String username, @Path("password") String password, @Body String location);
 
+    @PUT("/app/remove/replace/{id}/{username}/{password}")
+    Call<PrimerTube> removeAngGetNewPrimer(@Path("id") long id, @Path("username") String username, @Path("password") String password, @Body String message, @Body int status);
+
+    @PUT("/app/remove/{id}/{username}/{password}")
+    Call<Void> removePrimer(@Path("id") long id, @Path("username") String username, @Path("password") String password, @Body String message, @Body int status);
+
+    @GET("/app/returnPrimer/{id}/{username}/{password}")
+    Call<Void> returnPrimer(@Path("id") long id, @Path("username") String username, @Path("password") String password);
 
 
 }
