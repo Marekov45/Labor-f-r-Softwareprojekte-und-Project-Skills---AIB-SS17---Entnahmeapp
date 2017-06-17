147745aec19f09cd3cbb098cfeee3b3f235d1123
lhaeusse <lhaeusse@stud.hs-heilbronn.de>
2017-06-17 03:35:34 +0200

    Popup funktioniert, bei empty muss keine message eingetragen werden, bei den restliche Gründen schon, zurückButton implmentiert, neuer Primer wird noch nicht als Ersatz in die Liste eingetragen

--- app/src/main/java/client_aib_labswp_2017_ss_entnahmeapp/View/View/Pop.java
+++ app/src/main/java/client_aib_labswp_2017_ss_entnahmeapp/View/View/Pop.java
@@ -4,11 +4,11 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.DisplayMetrics;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.*;
 import client.aib_labswp_2017_ss_entnahmeapp.R;
 import client_aib_labswp_2017_ss_entnahmeapp.View.Controller.ServerAPI.CustomObserver;
 import client_aib_labswp_2017_ss_entnahmeapp.View.Controller.ServerAPI.PrimerImpl;
@@ -32,8 +32,12 @@ public class Pop extends AppCompatActivity implements CustomObserver {
     private TextView primerManufacturer;
     private TextView shownManufacturer;
     private TextView primerNote;
+    private EditText message;
 
     private Button submit;
+    private Button btnGoBack;
+    private RadioGroup reasonforNewPrimerGroup;
+    private RadioButton radioEmpty;
 
     private User uobj;
     private int positionGiven;
@@ -52,7 +56,7 @@ public class Pop extends AppCompatActivity implements CustomObserver {
         int width = dm.widthPixels;
         int height = dm.heightPixels;
 
-        getWindow().setLayout((int) (width * .8), (int) (height * .6));
+        getWindow().setLayout((int) (width * .8), (int) (height * .69));
 
         primerImpl = new PrimerImpl();
         primerImpl = new PrimerImpl();
@@ -67,7 +71,11 @@ public class Pop extends AppCompatActivity implements CustomObserver {
         primerManufacturer = (TextView) findViewById(R.id.tvManufacturerActTube);
         shownManufacturer = (TextView) findViewById(R.id.tvManufacturerActTube);
         primerNote = (TextView) findViewById(R.id.tvNote);
+        reasonforNewPrimerGroup = (RadioGroup) findViewById(R.id.reasonNewPrimerGroup);
+
+        message = (EditText) findViewById(R.id.editTextNote);
         submit = (Button) findViewById(R.id.btnSubmit);
+        btnGoBack=(Button) findViewById(R.id.btnclose);
 
         uobj = getIntent().getParcelableExtra("USER");
         tube = getIntent().getParcelableExtra("TUBE");
@@ -79,6 +87,34 @@ public class Pop extends AppCompatActivity implements CustomObserver {
         shownID.setText(tube.getPrimerTubeID());
         shownLOT.setText(tube.getLotNr());
         shownManufacturer.setText(tube.getManufacturer());
+        submit.setEnabled(true);
+
+        btnGoBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        reasonforNewPrimerGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.radioempty:
+                        message.setHint("Grund optional");
+                        submit.setEnabled(true);
+                        break;
+                    case R.id.radiobroken:
+                    case R.id.radionotfound:
+                    case R.id.radionoinfo:
+                        message.setText("");
+                        message.setHint("Grund zwingend nötig");
+                        submit.setEnabled(false);
+                        checkIfMessageEmpty();
+                        break;
+                }
+            }
+        });
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -87,12 +123,35 @@ public class Pop extends AppCompatActivity implements CustomObserver {
             }
         });
 
+
     }
 
-    private PrimerStatus createPrimerStatus() {
 
-        PrimerStatus status = new PrimerStatus("broken", 2);
-        return status;
+    private void checkIfMessageEmpty() {
+        message.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                if (s.toString().trim().length() == 0) {
+                    submit.setEnabled(false);
+                } else {
+                    submit.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.toString().trim().length() == 0) {
+                    submit.setEnabled(false);
+                } else {
+                    submit.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
     @Override
@@ -107,7 +166,36 @@ public class Pop extends AppCompatActivity implements CustomObserver {
     private void receiveNewPrimer(Object o) {
         Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show();
         PrimerTube primerTube = (PrimerTube) o;
-//        System.out.println(primerTube.getObjectID());
+    }
+
+    private int chooseReason() {
+
+        int selectedID = reasonforNewPrimerGroup.getCheckedRadioButtonId();
+
+        switch (selectedID) {
+            case R.id.radioempty:
+                return 1;
+            case R.id.radiobroken:
+                return 2;
+            case R.id.radionotfound:
+                return 5;
+            case R.id.radionoinfo:
+                return 199;
+        }
+        return 0;
+    }
+
+
+    private PrimerStatus createPrimerStatus() {
+        PrimerStatus status = new PrimerStatus("", 0);
+        if (message.getText().toString().matches("")) {
+            status.setMessage("");
+            status.setStatusCode(chooseReason());
+        } else {
+            status.setMessage(message.getText().toString());
+            status.setStatusCode(chooseReason());
+        }
+        return status;
     }
 
     @Override
