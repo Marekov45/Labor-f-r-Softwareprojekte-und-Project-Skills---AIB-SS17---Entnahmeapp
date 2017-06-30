7c04f27a86ac6f186ce7d0d6a4a38687020c7a2d
Marvin Rekovsky <rekovsky@stud.hs-heilbronn.de>
2017-06-30 22:18:34 +0200

    Added Javadoc for User and NewLocation class

--- app/src/main/java/client_aib_labswp_2017_ss_entnahmeapp/View/model/NewLocation.java
+++ app/src/main/java/client_aib_labswp_2017_ss_entnahmeapp/View/model/NewLocation.java
@@ -4,24 +4,51 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 /**
- * Created by User on 18.06.2017.
+ * Defines a {@link NewLocation} for a primer. The purpose of this class is that the location of a primer is
+ * immediately updated before loading the list another time.
  */
-public class NewLocation implements Parcelable{
+public class NewLocation implements Parcelable {
     private String newLocation;
-    public NewLocation(String location){
-        this.newLocation=location;
+
+    /**
+     * Initializes the {@link NewLocation}.
+     *
+     * @param location is not allowed to be {@code null} and must not be an empty
+     *                 String (<code>""</code>).
+     */
+    public NewLocation(String location) {
+        this.newLocation = location;
     }
 
+    /**
+     * Initializes the Parcel.
+     *
+     * @param in container for a message.
+     */
     protected NewLocation(Parcel in) {
         newLocation = in.readString();
     }
 
+    //Generates instances of this Parcelable class from a Parcel.
     public static final Creator<NewLocation> CREATOR = new Creator<NewLocation>() {
+        /**
+         * Create a new instance of the Parcelable class, instantiating it from the given Parcel whose data had
+         * previously been written by
+         *
+         * @param in the Parcel to read the object's data from.
+         * @return A new instance of the Parcelable class
+         */
         @Override
         public NewLocation createFromParcel(Parcel in) {
             return new NewLocation(in);
         }
 
+        /**
+         * Creates a new array of the Parcelable class.
+         *
+         * @param size size of the array.
+         * @return An array of the Parcelable class, with every entry initialized to null
+         */
         @Override
         public NewLocation[] newArray(int size) {
             return new NewLocation[size];
@@ -32,15 +59,30 @@ public class NewLocation implements Parcelable{
         return newLocation;
     }
 
+    /**
+     * @param newLocation is not allowed to be {@code null} and must not be an empty
+     *                    String (<code>""</code>).
+     */
     public void setNewLocation(String newLocation) {
         this.newLocation = newLocation;
     }
 
+    /**
+     * Describe the kinds of special objects contained in this Parcelable instance's marshaled representation.
+     *
+     * @return a bitmask indicating the set of special object types marshaled by this Parcelable object instance
+     */
     @Override
     public int describeContents() {
         return 0;
     }
 
+    /**
+     * Flattens the object in to a Parcel.
+     *
+     * @param dest  the Parcel in which the object should be written.
+     * @param flags additional flags about how the object should be written.
+     */
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(newLocation);
--- app/src/main/java/client_aib_labswp_2017_ss_entnahmeapp/View/model/User.java
+++ app/src/main/java/client_aib_labswp_2017_ss_entnahmeapp/View/model/User.java
@@ -4,21 +4,37 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 /**
- * Created by User on 08.06.2017.
+ * Defines a {@link User} with all of his information regarding the authentication.
  */
-public class User implements Parcelable{
+public class User implements Parcelable {
     private String username;
     private String password;
 
-    public User(String name, String password){
+    /**
+     * Initializes the {@link User}.
+     *
+     * @param name     is not allowed to be {@code null} and must not be an empty
+     *                 String (<code>""</code>).
+     * @param password is not allowed to be {@code null} and must not be an empty
+     *                 String (<code>""</code>).
+     */
+    public User(String name, String password) {
         this.username = name;
-        this.password=password;
+        this.password = password;
     }
 
+    /**
+     * @param username is not allowed to be {@code null} and must not be an empty
+     *                 String (<code>""</code>).
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * @param password is not allowed to be {@code null} and must not be an empty
+     *                 String (<code>""</code>).
+     */
     public void setPassword(String password) {
         this.password = password;
     }
@@ -32,28 +48,59 @@ public class User implements Parcelable{
         return password;
     }
 
+    /**
+     * Describe the kinds of special objects contained in this Parcelable instance's marshaled representation.
+     *
+     * @return a bitmask indicating the set of special object types marshaled by this Parcelable object instance
+     */
     @Override
     public int describeContents() {
         return 0;
     }
 
+    /**
+     * Flattens the object in to a Parcel.
+     *
+     * @param dest  the Parcel in which the object should be written.
+     * @param flags additional flags about how the object should be written.
+     */
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(this.username);
         dest.writeString(this.password);
     }
 
+    /**
+     * Initializes the Parcel.
+     *
+     * @param in container for a message.
+     */
     protected User(Parcel in) {
         this.username = in.readString();
         this.password = in.readString();
     }
 
+
+    //Generates instances of this Parcelable class from a Parcel.
     public static final Creator<User> CREATOR = new Creator<User>() {
+        /**
+         * Create a new instance of the Parcelable class, instantiating it from the given Parcel whose data had
+         * previously been written by
+         *
+         * @param source the Parcel to read the object's data from.
+         * @return A new instance of the Parcelable class
+         */
         @Override
         public User createFromParcel(Parcel source) {
             return new User(source);
         }
 
+        /**
+         * Creates a new array of the Parcelable class.
+         *
+         * @param size size of the array.
+         * @return An array of the Parcelable class, with every entry initialized to null
+         */
         @Override
         public User[] newArray(int size) {
             return new User[size];
